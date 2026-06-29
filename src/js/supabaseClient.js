9f01fa63db9400c08a8b7e130d0cac20747d73bd
Empirearcuz <BGroskurt@aol.de>
2026-06-29 12:54:11 +0200

    Delete src directory

--- src/js/supabaseClient.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = "https://jghvfulkqrvkdfgtnmkz.supabase.co";
-const supabaseKey = "sb_publishable_9ss9IZL1vsxLuhLToZePGA_mbzexjk5";
-const db = supabase.createClient(supabaseUrl, supabaseKey);
